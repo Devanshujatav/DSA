a227445be153daadb211f4f38ecb2cecddb93190
Devanshujatav <devanshujatav2003@gmail.com>
2025-05-27 15:20:12 +0000

    BubbleSort optimization

--- Sorting-Algorithms/Bubble-Sort/BubbleSort.java
+++ Sorting-Algorithms/Bubble-Sort/BubbleSort.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
-
-public class BubbleSort {
-
+public class BubbleSort{
     private static void bubbleSort(ArrayList<Integer> list){
+	boolean flag = false; 
         for (int i=0 ; i<list.size() ; i++){
             for (int j=0 ; j< list.size()-i-1 ; j++){
                 if (list.get(j) > list.get(j+1)){
+		    flag = true;			
                     int temp = list.get(j);
                     list.set(j , list.get(j+1));
                     list.set(j+1 , temp);
                 }
             }
+	    if(flag == false){
+	    	return;
+	    }
         }
     }
 

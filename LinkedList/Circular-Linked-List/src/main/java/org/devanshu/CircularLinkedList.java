e83b2324defbfa204972921ee9fc34dd57451c2d
Devanshu Jatav <devanshujatav2003@gmail.com>
2025-09-26 19:48:03 +0530

    CircularLinkedList

---
+++ LinkedList/Circular-Linked-List/src/main/java/org/devanshu/CircularLinkedList.java
@@ -0,0 +1,25 @@
+package org.devanshu;
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(){
+        super();
+        // Default Constructor
+    }
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class CircularLinkedList {
+
+
+    public Node add(Node head){
+
+        return head;
+    }
+}
